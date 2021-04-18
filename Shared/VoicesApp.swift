0b0fd9b154d4c2e1372143fe22f7b0de5d11a575
radibaraq <radibaraq@cloud.upwork.com>
2021-04-18 18:43:52 +0300

    Initial Commit

---
+++ Shared/VoicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesApp.swift
+//  Shared
+//
+//  Created by Radi Barq on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct VoicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
